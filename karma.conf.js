6c40b3389b626b836c4f3f6d6a37cf45155e194a
Adrian Juszczak <adrian@juszczak.io>
2016-06-16 21:15:19 +0200

    karma config cleanup
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,14 @@
 'use strict';
 
 const path = require('path');
+const webpackConfig = require('./webpack.config');
 
 const ENV_PRODUCTION = process.env.KARMA_ENV === 'production';
 
-let config = (config) => {
+module.exports = function(config) {
   config.set({
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-    // list of files / patterns to load in the browser
     files: [{
         pattern: 'node_modules/zone.js/dist/zone.js',
         included: true,
@@ -33,7 +30,7 @@ let config = (config) => {
         included: false,
         watched: false
       }, {
-        pattern: 'node_modules/angular2/**/*.js',
+        pattern: 'node_modules/@angular/**/*.js',
         included: false,
         watched: false
       }, {
@@ -41,78 +38,20 @@ let config = (config) => {
         included: true
       }
     ],
-    // list of files to exclude
     exclude: [],
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'src/**/*.spec.ts': ['webpack', 'sourcemap']
     },
-    webpack: {
-      devtool: 'inline-source-map',
-      resolve: {
-        root: [path.join(__dirname, 'src')],
-        extensions: ['', '.ts', '.js'],
-      },
-      module: {
-        loaders: [{
-          test: /\.json$/,
-          loader: 'json'
-        }, {
-          test: /\.ts$/,
-          loader: 'awesome-typescript'
-        }, {
-          test: /\.html$/,
-          loader: 'raw',
-          exclude: [
-            path.resolve(__dirname, './src/index.html'),
-          ]
-        }, {
-          test: /\.styl$/,
-          loader: 'raw!postcss!stylus',
-          exclude: [
-            path.resolve(__dirname, './src/global.styl'),
-          ]
-        }, {
-          test: /\.(png|jpe?g)$/i,
-          loader: 'file',
-          query: {
-            hash: 'sha512',
-            digest: 'hex',
-            name: 'assets/[name].[hash].[ext]'
-          }
-        }]
-      },
-      stats: {
-        colors: true,
-        reasons: true
-      },
-      debug: false
-    },
+    webpack: webpackConfig,
     webpackMiddleware: {
-      // please do not spam the console when running in karma!
       noInfo: true
     },
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['mocha'],
-    // web server port
     port: 9876,
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-    // enable / disable watching file and executing tests whenever any file changes
+    logLevel: config.LOG_DISABLE,
     autoWatch: ENV_PRODUCTION ? false : true,
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: ENV_PRODUCTION ? true : false
   });
 };
-
-module.exports = config;
