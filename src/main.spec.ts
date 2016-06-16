1edaef326b870c481f60b126f2560d99f918d7fb
Adrian Juszczak <adrian@juszczak.io>
2016-06-16 21:15:46 +0200

    should be able to test spec
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

---
+++ src/main.spec.ts
@@ -0,0 +1,11 @@
+import {
+  it,
+  describe,
+  expect,
+} from '@angular/core/testing';
+
+describe('[test] main.spec.ts', () => {
+  it('should be able to test', () => {
+    expect(true).toBe(true);
+  });
+});
